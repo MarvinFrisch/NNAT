a2a3decaee36977c478feb8974d71dee24202763
MarvinFrisch <marvin.frisch@kit.edu>
2023-12-21 14:06:57 +0000

    Changed func to pass through exponents Created models try to fit to x**0,x**1,..., results are plotted

--- src/main.py
+++ src/main.py
@@ -1,33 +1,47 @@
 import tensorflow as tf
-import time
+import os
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
 import matplotlib.pyplot as plt
 
-input = [t*0.001 for t in range(0, 1000)]
+input = [t*0.001 for t in range(-1000, 1000)]
 
-max_number_of_neurons = 20
-number_of_passes = 20
+max_number_of_neurons = 5
+max_exponent = 5
 
-for number_of_neurons in range(1,max_number_of_neurons+1):
+activation_function = "linear"
+
+for exponent in range(max_exponent):
+    print(f"x**{exponent}")
+    true_output = [i**exponent for i in input]
+
     plt.figure()
-    for pass_number in range(number_of_passes):
-        initializer = tf.keras.initializers.RandomNormal(mean=0., stddev=10.)
+    plt.xlim([-1.2,1.2])
+    plt.ylim([-1.2,1.2])
+    plt.plot(input, true_output, color='b', label="True")
+    for number_of_neurons in range(1,max_number_of_neurons+1):
+        print(f"    1-{number_of_neurons}-1")
+        
+
+        initializer = tf.keras.initializers.RandomNormal(mean=0., stddev=1.)
 
         model = Sequential()
-        model.add(Dense(number_of_neurons, input_shape=(1,), activation="sigmoid", kernel_initializer=initializer))
-        model.add(Dense(1, activation="sigmoid"))
+        model.add(Dense(number_of_neurons, input_shape=(1,), activation=activation_function, kernel_initializer=initializer))
+        model.add(Dense(1, activation="tanh"))
 
-        model.compile()
+        model.compile(loss='mse')
 
-        output = model.predict([[i] for i in input])
+        model.fit(input, true_output, epochs=100, verbose=0)
 
-        min_output = min(output)
-        max_output= max(output)
-        output = [(i-min_output)/(max_output-min_output) for i in output]
+        print(f"    Weigths: {model.layers[0].get_weights()[0]} , Bias: {model.layers[0].get_weights()[1]}")
+        print(f"    Weigths: {model.layers[1].get_weights()[0]} , Bias: {model.layers[1].get_weights()[1]}")
 
-        plt.plot(input, output)
-    plt.savefig(f"1_Hidden_with_{number_of_neurons}_neurons.png")
+        predicted_output = model.predict([[i] for i in input], verbose=0)
+
+        plt.plot(input, predicted_output, label=f"1-{number_of_neurons}-1")
+
+    plt.legend()
+    plt.savefig(f"exponent_{exponent}.png")
     plt.close()
